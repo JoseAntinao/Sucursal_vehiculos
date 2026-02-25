41a691a3b21a2209c365756176d04b5849f037f2
JoseAntinao <j.antinao@duocuc.cl>
2026-02-25 04:49:29 -0300

    Add CRUD operations to VehiculoController

--- src/main/java/com/vehiculos/VehiculosRest/controllers/VehiculoController.java
+++ src/main/java/com/vehiculos/VehiculosRest/controllers/VehiculoController.java
@@ -1,41 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.vehiculos.VehiculosRest.controllers;
-
-import com.vehiculos.VehiculosRest.models.VehiculoModel;
-import com.vehiculos.VehiculosRest.services.VehiculoService;
-import com.vehiculos.VehiculosRest.services.VehiculoService.UserNotFoundException;
-import java.util.ArrayList;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- *
- * @author Villacura
- */
 @RestController
 @RequestMapping("/vehiculos")
 public class VehiculoController {
     
-    
     @Autowired
     private VehiculoService vehiculoService;
     
+    // LISTAR (El que ya tienes)
     @GetMapping
     public ArrayList<VehiculoModel> getAutos(){
         return this.vehiculoService.getVehiculos();
     }
-   
+
+    // GUARDAR (Para solucionar el error 405 en Postman)
+    @PostMapping
+    public ResponseEntity<VehiculoModel> guardarVehiculo(@RequestBody VehiculoModel vehiculo) {
+        VehiculoModel nuevo = this.vehiculoService.saveVehiculo(vehiculo);
+        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
+    }
+
+    // ELIMINAR
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id) {
+        boolean ok = this.vehiculoService.deleteVehiculo(id);
+        if (ok) {
+            return "Vehículo eliminado con éxito";
+        } else {
+            return "No se pudo eliminar el vehículo";
+        }
+    }
 }
